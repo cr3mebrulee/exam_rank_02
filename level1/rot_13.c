1a0bd1a93c836236869c0885a3337622ec2f7c3f
Tatiana Retiukhina <taretiuk@c4b5c4.42berlin.de>
2024-03-02 15:02:52 +0100

    one more genius solution

---
+++ level1/rot_13.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+char	replace_letter(char c)
+{
+	if ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm'))
+		c += 13;
+	else if ((c >= 'N' && c <= 'Z') || (c >= 'n' && c <= 'z'))
+		c -= 13;
+	return (c);
+}
+
+int	main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	int	i;
+	char	c;
+
+	i = 0;
+	while(argv[1][i] != '\0')
+	{
+		c = replace_letter(argv[1][i]);
+		write(1, &c, 1);
+		i++;
+	}
+	return (0);
+}
