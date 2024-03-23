5214fe1ef8357aff4b6b7b7be78cb4b0abd9e29f
Tatiana Retiukhina <taretiuk@c4c3c2.42berlin.de>
2024-03-23 14:55:06 +0100

    modified

--- level3/ft_rrange.c
+++ level3/ft_rrange.c
@@ -1,28 +1,37 @@
 #include <stdlib.h>
 
-int *ft_rrange(int start, int end) {
-    // calculate the length
-    int length = abs(end - start) + 1;
+int     *ft_rrange(int start, int end)
+{
+	int	length = abs(end - start) + 1;
+	int	*arr = (int *)malloc(sizeof(int) * length);
+	if(!arr)
+		return(0);
 
-    // allocate memory
-    int *result = (int *)malloc(length * sizeof(int));
-    if (result == NULL) {
-        return NULL;
-    }
+	int	direction = (end > start) ? 1 : -1;
 
-    // determine the direction of the array
-    int direction = (end > start) ? 1 : -1;
-
-    // fill the array
-    int i = 0;
-    int current = start;
-    while (i < length) {
-        result[i] = current;
-        current += direction;
-        i++;
-    }
-
-    // retrun the pointer to the first element of the array
-    return result;
+	int	i = 0;
+	int	current = end;
+	while(i < length)
+	{
+		arr[i] = current;
+		current -= direction;
+		i++;
+	}
+	return(arr);
 }
-
+/*
+#include <stdio.h>
+int	main()
+{
+	int	start = 3;
+	int	end = -9;
+	int	*rev_arr = ft_rrange(start, end);
+	int	lenght = 0;
+	int	i = 0;
+	while(rev_arr[i] != '!')
+	{
+		printf("%d", rev_arr[i]);
+		i++;
+	}
+}
+*/
